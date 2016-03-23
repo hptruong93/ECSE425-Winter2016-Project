fab2bccb6ace21505ddbefedd15c245b16605197
jjc10 <chataouijoud@gmail.com>
2016-03-22 21:36:29 -0400

    Added gitignore and allowed multiple tests in run.tcl

--- run.tcl
+++ run.tcl
@@ -16,9 +16,9 @@ vcom test_alu.vhd
 vcom test_writeback.vhd
 
 ;#Start a simulation session with the fsm_tb component
-# vsim -t ps alu_tb
-vsim -t ps writeback_tb
-
+vsim -t ps alu_tb
 force -deposit clk 0 0 ns, 1 0.5 ns -repeat 1 ns
+run 20ns
 
-run 30ns
+vsim -t ps writeback_tb
+run 20ns
