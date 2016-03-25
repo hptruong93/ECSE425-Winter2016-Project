1fb39b5ef5621b99278ed15cea2f4c795231a6ca
jjc10 <chataouijoud@gmail.com>
2016-03-25 17:51:00 -0400

    Wired all components in MasterPipeline. Added new signals to memstage

--- MasterPipeline.vhd
+++ MasterPipeline.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use work.register_array.all;
+use work.memory_arbiter_lib.all;
 
 entity MasterPipeline is
 port (	clk 	: in STD_LOGIC;
@@ -13,9 +14,10 @@ port (	clk 	: in STD_LOGIC;
 			re1			: out STD_LOGIC;
 			busy1 		: in STD_LOGIC;
 
-			store_load address		: out NATURAL; -- fed to port 2 of mem arbiter
+			store_load_address : out NATURAL; -- fed to port 2 of mem arbiter
 			input_memory_data	: in STD_LOGIC_VECTOR(MEM_DATA_WIDTH-1 downto 0); -- for load
 			output_memory_data : out STD_LOGIC_VECTOR(MEM_DATA_WIDTH-1 downto 0); -- for store
+			word_byte : out STD_LOGIC; -- send to arbiter to control whether we interact in bytes or words
 			re2			: out STD_LOGIC;
 			we2			: out STD_LOGIC;
 			busy2 		: in STD_LOGIC
@@ -24,17 +26,21 @@ end MasterPipeline;
 
 architecture behavioral of MasterPipeline is
 
-COMPONENT ALU IS
-	port (	clk 	: in STD_LOGIC;
+COMPONENT InstructionFetch is
+port (	clk 	: in STD_LOGIC;
 			reset : in STD_LOGIC;
-			
-			data1	:	in signed(32-1 downto 0);
-			data2	:	in signed(32-1 downto 0);
 
-			operation : in STD_LOGIC_VECTOR(6-1 downto 0);
-			lo_reg : out signed (32-1 downto 0);
-			hi_reg : out signed (32-1 downto 0);
-			result : out signed(32-1 downto 0)
+			branch_signal : in  STD_LOGIC_VECTOR(2-1 downto 0);
+			branch_address : in STD_LOGIC_VECTOR(32-1 downto 0); --from decoder
+			data : in STD_LOGIC_VECTOR(32-1 downto 0); --from memory
+
+			is_mem_busy : in STD_LOGIC;
+
+			pc_reg : out STD_LOGIC_VECTOR(32-1 downto 0); --send to decoder
+			do_read : out STD_LOGIC;
+			address : out STD_LOGIC_VECTOR(32-1 downto 0); --address to fetch the next instruction
+			is_busy : out STD_LOGIC; --if waiting for memory
+			instruction : out STD_LOGIC_VECTOR(32-1 downto 0) --instruction send to decoder
 	);
 end COMPONENT;
 
@@ -59,31 +65,98 @@ port (	clk 	: in STD_LOGIC;
 	);
 end COMPONENT;
 
+COMPONENT ALU IS
+	port (	clk 	: in STD_LOGIC;
+			reset : in STD_LOGIC;
+			
+			data1	:	in signed(32-1 downto 0);
+			data2	:	in signed(32-1 downto 0);
+
+			operation : in STD_LOGIC_VECTOR(6-1 downto 0);
+			lo_reg : out signed (32-1 downto 0);
+			hi_reg : out signed (32-1 downto 0);
+			result : out signed(32-1 downto 0)
+	);
+end COMPONENT;
+
+
+COMPONENT MemStage is
+port (	clk 	: in STD_LOGIC;
+			reset : in STD_LOGIC;
+
+			mem_address : in SIGNED(32-1 downto 0); -- coming from ALU
+			mem_writeback_register : in STD_LOGIC_VECTOR(5-1 downto 0); -- used for store, tells which register to read from.
+			registers : in register_array;
+			signal_to_mem : in STD_LOGIC_VECTOR(3-1 downto 0);
+			is_mem_busy : in STD_LOGIC;
+
+			word_byte: out STD_LOGIC; --  when '1' you are interacting with the memory in word otherwise in byte
+			do_read : out STD_LOGIC;
+			do_write : out	STD_LOGIC;
+			is_busy : out STD_LOGIC;
+			address_line : out NATURAl;
+			input_data_line : in STD_LOGIC_VECTOR(32-1 downto 0); -- received and sent from/to memory arbiter
+			output_data_line : out STD_LOGIC_VECTOR(32-1 downto 0);
+			mem_stage_output : out STD_LOGIC_VECTOR(32-1 downto 0)
+	);
+end COMPONENT;
+
+COMPONENT WriteBack is
+port (	clk 	: in STD_LOGIC;
+			reset : in STD_LOGIC;
+			
+			lo_reg : in signed (32-1 downto 0);
+			hi_reg : in signed (32-1 downto 0);
+
+			writeback_source : in STD_LOGIC_VECTOR(3-1 downto 0); --sent from decoder
+			alu_output : in signed(32-1 downto 0);
+			mem_stage_busy : in STD_LOGIC;
+			mem_stage_output : in STD_LOGIC_VECTOR(32-1 downto 0);
+			mem_writeback_register : in STD_LOGIC_VECTOR(5-1 downto 0); --sent fromm decoder
+
+			registers : out register_array
+	);
+end COMPONENT;
+
 signal operation : STD_LOGIC_VECTOR(6-1 downto 0); -- Decoder => ALU
 signal instruction : STD_LOGIC_VECTOR(32-1 downto 0); -- Fetch unit ==> Decoder
-signal data1 : STD_LOGIC_VECTOR(32-1 downto 0);
-signal data2 : STD_LOGIC_VECTOR(32-1 downto 0);
-signal lo_reg : signed (32-1 downto 0);
-signal hi_reg : signed (32-1 downto 0);
+signal data1 : STD_LOGIC_VECTOR(32-1 downto 0); -- Decoder ==> ALU
+signal data2 : STD_LOGIC_VECTOR(32-1 downto 0); -- Decoder ==> ALU
+signal signed_data1 : signed(32-1 downto 0); -- Decoder ==> ALU
+signal signed_data2 : signed(32-1 downto 0); -- Decoder ==> ALU
+
+signal lo_reg : signed (32-1 downto 0); -- ALU ==> Mem unit
+signal hi_reg : signed (32-1 downto 0); -- ALU ==> Mem unit
 signal result : signed(32-1 downto 0); -- ALU ==> Mem unit
 signal registers : register_array;
-signal pc_reg : STD_LOGIC_VECTOR(32-1 downto 0);
+signal pc_reg : STD_LOGIC_VECTOR(32-1 downto 0); -- Fetch ==> Decode
+signal instruction_address_output : STD_LOGIC_VECTOR(32-1 downto 0);
 signal mem_writeback_register : STD_LOGIC_VECTOR(5-1 downto 0); -- Decoder ==> Write back unit
 signal signal_to_mem : STD_LOGIC_VECTOR(3-1 downto 0);
 signal writeback_source : STD_LOGIC_VECTOR(3-1 downto 0);
 signal branch_signal : STD_LOGIC_VECTOR(2-1 downto 0); --send to branch
 signal branch_address : STD_LOGIC_VECTOR(32-1 downto 0);
+signal mem_stage_busy : STD_LOGIC;
+signal instruction_fetch_busy : STD_LOGIC;
+signal mem_stage_output : STD_LOGIC_VECTOR(32-1 downto 0);
 
 begin
-	ALU_instance: ALU port map (
+	instruction_address <= to_integer(unsigned(instruction_address_output));
+	fetch_instance : InstructionFetch port map (
 		clk => clk,
 		reset => reset,
-		data1 => data1,
-		data2 => data2,
-		operation => operation,
-		lo_reg => lo_reg,
-		hi_reg => hi_reg,
-		result => result
+
+ 		branch_signal => branch_signal,
+		branch_address => branch_address,
+		data => fetched_instruction,
+
+ 		is_mem_busy => busy1,
+
+ 		pc_reg => pc_reg,
+		do_read => re1,
+		address => instruction_address_output,--STD_LOGIC_VECTOR(unsigned(instruction_address_output, 32)),
+		is_busy => instruction_fetch_busy, -- memory is busy, cannot fetch instruction
+		instruction => instruction
 	);
 
 	decoder_instance : Decoder port map (
@@ -98,9 +171,60 @@ begin
 		writeback_source => writeback_source,
 		branch_signal => branch_signal,
 		branch_address => branch_address,
-		data1 =>data1,
+		data1 => data1,
 		data2 => data2
-	); 
+	);
+
+
+	signed_data1 <= signed(data1);
+	signed_data2 <= signed(data2);
+	ALU_instance: ALU port map (
+		clk => clk,
+		reset => reset,
+		data1 => signed_data1,
+		data2 => signed_data2,
+		operation => operation,
+		lo_reg => lo_reg,
+		hi_reg => hi_reg,
+		result => result
+	);
+
+
+	mem_stage_instance : MemStage port map (
+		clk => clk,
+		reset => reset,
+		mem_address => result, -- coming from alu
+		mem_writeback_register => mem_writeback_register,
+		registers => registers,
+		signal_to_mem => signal_to_mem,
+		is_mem_busy => busy2, -- input from memory, check if channel 2 (for data) is busy
+
+
+		word_byte => word_byte,
+		do_read => re2,
+		do_write => we2,
+		is_busy => mem_stage_busy,
+		address_line => store_load_address,
+		input_data_line => input_memory_data,
+		output_data_line => output_memory_data,
+		mem_stage_output => mem_stage_output
+	);
+
+	writeback_instance : WriteBack port map (
+		clk => clk, 
+		reset => reset,
+
+		lo_reg => lo_reg,
+		hi_reg => hi_reg,
+
+		writeback_source => writeback_source,
+		alu_output => result,
+		mem_stage_busy => mem_stage_busy,
+		mem_stage_output => mem_stage_output,
+		mem_writeback_register => mem_writeback_register,
+
+		registers => registers
+	);
 
 
 	synced_clock : process(clk, reset)
--- test_memstage.vhd
+++ test_memstage.vhd
@@ -12,27 +12,25 @@ ARCHITECTURE behaviour OF memstage_tb IS
 SIGNAL clk, reset: STD_LOGIC := '0';
 CONSTANT clk_period : time := 1 ns;
 
-CONSTANT DUMMY_32_ONE : 	signed(32-1 downto 0) := "01010110101011010010100010010010";
-CONSTANT DUMMY_32_TWO : 	signed(32-1 downto 0) := "00100100110101010110110101100101";
-CONSTANT DUMMY_32_THREE : 	signed(32-1 downto 0) := "11010110101001000011110101010101";
-
 COMPONENT MemStage IS
 					port (	clk 	: in STD_LOGIC;
-					reset : in STD_LOGIC;
+			reset : in STD_LOGIC;
 
-					mem_address : in SIGNED(32-1 downto 0); -- coming from ALU
-					--operation : out STD_LOGIC_VECTOR(6-1 downto 0);
-					mem_writeback_register : in STD_LOGIC_VECTOR(5-1 downto 0); -- used for store, tells which register to read from.
-					registers : in register_array;
-					signal_to_mem : in STD_LOGIC_VECTOR(3-1 downto 0);
+			mem_address : in SIGNED(32-1 downto 0); -- coming from ALU
+			--operation : out STD_LOGIC_VECTOR(6-1 downto 0);
+			mem_writeback_register : in STD_LOGIC_VECTOR(5-1 downto 0); -- used for store, tells which register to read from.
+			registers : in register_array;
+			signal_to_mem : in STD_LOGIC_VECTOR(3-1 downto 0);
+			is_mem_busy : in STD_LOGIC;
 
-					is_mem_busy : in STD_LOGIC;
-					do_read : out STD_LOGIC;
-					do_write : out	STD_LOGIC;
-					word_byte: out STD_LOGIC; --  when '1' you are interacting with the memory in word otherwise in byte
-					is_busy : out STD_LOGIC;
-					address_line : out NATURAl;
-					data_line : inout STD_LOGIC_VECTOR(32-1 downto 0) -- received and sent from/to memory arbiter
+			word_byte: out STD_LOGIC; --  when '1' you are interacting with the memory in word otherwise in byte
+			do_read : out STD_LOGIC;
+			do_write : out	STD_LOGIC;
+			is_busy : out STD_LOGIC;
+			address_line : out NATURAl;
+			input_data_line : in STD_LOGIC_VECTOR(32-1 downto 0); -- coming from memory arbiter
+			output_data_line : out STD_LOGIC_VECTOR(32-1 downto 0); -- sending to memory arbiter
+			mem_stage_output : out STD_LOGIC_VECTOR(32-1 downto 0)
 	);
 end COMPONENT;
 
@@ -48,9 +46,12 @@ signal do_read : STD_LOGIC;
 signal do_write : STD_LOGIC;
 signal is_busy : STD_LOGIC;
 signal word_byte : STD_LOGIC;
-signal data_line : STD_LOGIC_VECTOR(32-1 downto 0);
+signal input_data_line : STD_LOGIC_VECTOR(32-1 downto 0);
+signal output_data_line : STD_LOGIC_VECTOR(32-1 downto 0);
+signal mem_stage_output : STD_LOGIC_VECTOR(32-1 downto 0);
 signal address_line : NATURAL;
 
+signal count : STD_LOGIC := '0';
 
 BEGIN
 	test_bench: MemStage	
@@ -69,7 +70,9 @@ BEGIN
 						word_byte => word_byte,
 						is_busy => is_busy,
 						address_line => address_line,
-						data_line => data_line
+						input_data_line => input_data_line,
+						output_data_line => output_data_line,
+						mem_stage_output => mem_stage_output
 			);
 
 	 --clock process
@@ -85,6 +88,7 @@ BEGIN
 	--TODO: Thoroughly test the crap
 	stim_process: PROCESS
 	BEGIN
+		if count = '0' then count <= '1';
 		WAIT FOR 1 * clk_period; --clock high first
 
 		reset <= '1';
@@ -96,6 +100,7 @@ BEGIN
 		signal_to_mem <= LOAD_WORD;
 		is_mem_busy <= '0';
 		mem_address <= DUMMY_32_ONE;
+		input_data_line <= STD_LOGIC_VECTOR(DUMMY_32_TWO);
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '1') REPORT "is_busy should be 1" SEVERITY ERROR;
 		ASSERT (do_read = '1') REPORT "do_read should be 1" SEVERITY ERROR;
@@ -103,11 +108,13 @@ BEGIN
 		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
+		ASSERT (mem_stage_output = STD_LOGIC_VECTOR(DUMMY_32_TWO)) REPORT "mem_stage_output is wrong" SEVERITY ERROR;
 
 		REPORT "################### 2. LOAD WORD, BUSY MEM";
 		signal_to_mem <= LOAD_WORD;
 		is_mem_busy <= '1';
 		mem_address <= DUMMY_32_ONE;
+		input_data_line <= STD_LOGIC_VECTOR(DUMMY_32_TWO);
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '1') REPORT "is_busy should be 1, waiting on memory" SEVERITY ERROR;
 		ASSERT (do_read = '0') REPORT "do_read should be 0, waiting on memory" SEVERITY ERROR;
@@ -119,6 +126,7 @@ BEGIN
 		ASSERT (word_byte = '1') REPORT "word_byte should be 1" SEVERITY ERROR;
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
+		ASSERT (mem_stage_output = STD_LOGIC_VECTOR(DUMMY_32_TWO)) REPORT "mem_stage_output is wrong" SEVERITY ERROR;
 
 		REPORT "################### 3. STORE WORD, AVAILABLE MEM";
 		signal_to_mem <= STORE_WORD;
@@ -131,7 +139,7 @@ BEGIN
 		ASSERT (do_write = '1') REPORT "do_read should be 1" SEVERITY ERROR;
 		ASSERT (word_byte = '1') REPORT "word_byte should be 1" SEVERITY ERROR;
 		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
-		ASSERT (data_line = registers(to_integer(unsigned(mem_writeback_register))));
+		ASSERT (output_data_line = registers(to_integer(unsigned(mem_writeback_register))));
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
 
@@ -150,7 +158,7 @@ BEGIN
 		ASSERT (do_write = '1') REPORT "do_read should be 1" SEVERITY ERROR;
 		ASSERT (word_byte = '1') REPORT "word_byte should be 1" SEVERITY ERROR;
 		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
-		ASSERT (data_line = registers(to_integer(unsigned(mem_writeback_register))));
+		ASSERT (output_data_line = registers(to_integer(unsigned(mem_writeback_register))));
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
 
@@ -158,6 +166,7 @@ BEGIN
 		signal_to_mem <= LOAD_BYTE;
 		is_mem_busy <= '0';
 		mem_address <= DUMMY_32_ONE;
+		input_data_line <= "00000000000000000000000000000010";
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '1') REPORT "is_busy should be 1" SEVERITY ERROR;
 		ASSERT (do_read = '1') REPORT "do_read should be 1" SEVERITY ERROR;
@@ -165,12 +174,27 @@ BEGIN
 		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
+		ASSERT (mem_stage_output = "00000000000000000000000000000010") REPORT "mem_stage_output is wrong" SEVERITY ERROR;
 
+		REPORT "################### 5b. LOAD BYTE, AVAILABLE MEM, NEGATIVE LOAD";
+		signal_to_mem <= LOAD_BYTE;
+		is_mem_busy <= '0';
+		mem_address <= DUMMY_32_ONE;
+		input_data_line <= "00000000000000000000000010000010";
+		WAIT FOR 1 * clk_period;
+		ASSERT (is_busy = '1') REPORT "is_busy should be 1" SEVERITY ERROR;
+		ASSERT (do_read = '1') REPORT "do_read should be 1" SEVERITY ERROR;
+		ASSERT (word_byte = '0') REPORT "word_byte should be 0" SEVERITY ERROR;
+		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
+		WAIT FOR 1 * clk_period;
+		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
+		ASSERT (mem_stage_output = "11111111111111111111111110000010") REPORT "mem_stage_output is wrong" SEVERITY ERROR;
 
 		REPORT "################### 6. LOAD BYTE, BUSY MEM";
 		signal_to_mem <= LOAD_BYTE;
 		is_mem_busy <= '1';
 		mem_address <= DUMMY_32_ONE;
+		input_data_line <= "00000000000000000000000000000010";
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '1') REPORT "is_busy should be 1, waiting on memory" SEVERITY ERROR;
 		ASSERT (do_read = '0') REPORT "do_read should be 0, waiting on memory" SEVERITY ERROR;
@@ -182,6 +206,7 @@ BEGIN
 		ASSERT (word_byte = '0') REPORT "word_byte should be 0" SEVERITY ERROR;
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
+		ASSERT (mem_stage_output = "00000000000000000000000000000010") REPORT "mem_stage_output is wrong" SEVERITY ERROR;
 
 
 		REPORT "################### 7. STORE BYTE, AVAILABLE MEM";
@@ -195,7 +220,7 @@ BEGIN
 		ASSERT (do_write = '1') REPORT "do_read should be 1" SEVERITY ERROR;
 		ASSERT (word_byte = '0') REPORT "word_byte should be 0" SEVERITY ERROR;
 		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
-		ASSERT (data_line = registers(to_integer(unsigned(mem_writeback_register))));
+		ASSERT (output_data_line = registers(to_integer(unsigned(mem_writeback_register))));
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
 
@@ -214,10 +239,14 @@ BEGIN
 		ASSERT (do_write = '1') REPORT "do_read should be 1" SEVERITY ERROR;
 		ASSERT (word_byte = '0') REPORT "word_byte should be 0" SEVERITY ERROR;
 		ASSERT (address_line = DUMMY_32_ONE) REPORT "Wrong address on address line" SEVERITY ERROR;
-		ASSERT (data_line = registers(to_integer(unsigned(mem_writeback_register))));
+		ASSERT (output_data_line = registers(to_integer(unsigned(mem_writeback_register))));
 		WAIT FOR 1 * clk_period;
 		ASSERT (is_busy = '0') REPORT "is_busy should be 0, access is done" SEVERITY ERROR;
 		
-
+		REPORT "#####################################################End test";
+		else
+			count <= '1';
+			WAIT FOR 100 * clk_period;
+		end if;
 	END PROCESS;
 END;
