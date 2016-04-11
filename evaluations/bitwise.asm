d19ae1b091e1565d78e51428e4585059ae3ca7bd
hptruong93 <hptruong93@gmail.com>
2016-04-11 17:53:24 -0400

    added labels and lines to bitewise.asm

--- evaluations/bitwise.asm
+++ evaluations/bitwise.asm
@@ -7,57 +7,58 @@
 # $6: b = x || y   */ logical OR: at least one is nonzero, so w = 1 /*
 
 
-				addi $11,  $0, 2000  	# initializing the beginning of Data Section address in memory
-				addi $15, $0, 4 		# word size in byte
+				addi $11,  $0, 2000  	# initializing the beginning of Data Section address in memory 	#0
+				addi $15, $0, 4 		# word size in byte 										   	#4
 				
-Bitwise:        addi $1, $0, 3
-                addi $2, $0, 4
-                and  $3, $1, $2         # z = x & y
+Bitwise:        addi $1, $0, 3																			#8
+                addi $2, $0, 4																			#12
+                and  $3, $1, $2         # z = x & y														#16
 				
-				addi $10, $0, 0
-				mult $10, $15			# $lo=4*$10, for word alignment 
-				mflo $12				# assume small numbers
-				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
-				add $2,$0,$3 
-				sw	 $2, 0($13)
+				addi $10, $0, 0																			#20
+				mult $10, $15			# $lo=4*$10, for word alignment 								#24
+				mflo $12				# assume small numbers											#28
+				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]								#32
+				add $2,$0,$3 																			#36
+				sw	 $2, 0($13)																			#40
 
-                # w = x && y
-                beq  $1, $0, False      # branch to False if x = 0
-                beq  $2, $0, False      # branch to False if y = 0
-                addi $4, $0, 1          # x and y are both nonzero, so w = 1
+                # w = x && y						
+                beq  $1, $0, False      # branch to False if x = 0										#44
+                beq  $2, $0, False      # branch to False if y = 0										#48
+                addi $4, $0, 1          # x and y are both nonzero, so w = 1							#52
 				
-				addi $10, $0, 1
-				mult $10, $15			# $lo=4*$10, for word alignment 
-				mflo $12				# assume small numbers
-				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
-				add $2,$0,$4 
-				sw	 $2, 0($13)
+				addi $10, $0, 1																			#56
+				mult $10, $15			# $lo=4*$10, for word alignment 								#60
+				mflo $12				# assume small numbers											#64
+				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]								#68
+				add $2,$0,$4 																			#72
+				sw	 $2, 0($13)																			#76
 				
-                j Continue
-False:          addi $4, $0, 0          # x and/or y are 0, so w = 0
+                j Continue																				#80
+False:          addi $4, $0, 0          # x and/or y are 0, so w = 0									#84
 
-Continue:       or   $5, $1, $2         # a = x | y
+Continue:       or   $5, $1, $2         # a = x | y														#88
 				
-				addi $10, $0, 3
-				mult $10, $15			# $lo=4*$10, for word alignment 
-				mflo $12				# assume small numbers
-				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
-				add $2,$0,$5 
-				sw	 $2, 0($13)
+				addi $10, $0, 3																			#92
+				mult $10, $15			# $lo=4*$10, for word alignment 								#96
+				mflo $12				# assume small numbers											#100
+				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]								#104
+				add $2,$0,$5 																			#108
+				sw	 $2, 0($13)																			#112
 				
                 # w = x || y
-                bne  $1, $0, True       # branch to True if x is non-zero
-                bne  $2, $0, True       # branch to True if y is non-zero
-                addi $6, $0, 0          # x and y are both zero, so b = 0
+                bne  $1, $0, True       # branch to True if x is non-zero								#116
+                bne  $2, $0, True       # branch to True if y is non-zero								#120
+                addi $6, $0, 0          # x and y are both zero, so b = 0								#124
 				
-				addi $10, $0, 4
-				mult $10, $15			# $lo=4*$10, for word alignment 
-				mflo $12				# assume small numbers
-				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]
-				add $2,$0,$6 
-				sw	 $2, 0($13)
+				addi $10, $0, 4																			#128
+				mult $10, $15			# $lo=4*$10, for word alignment 								#132
+				mflo $12				# assume small numbers											#136
+				add  $13, $11, $12 		# Make data pointer [2000+($10)*4]								#140
+				add $2,$0,$6 																			#144
+				sw	 $2, 0($13)																			#148
 				
-                j End
-True:           addi $6, $0, 1          # x and/or y are non-zero, so b = 1
+                j End 																					#152
+True:           addi $6, $0, 1          # x and/or y are non-zero, so b = 1								#156
 
-End:       		beq	 $11, $11, End 		#end of program (infinite loop)
+End:       		add  $31, $31, $0 		#evaluation purpose												#160
+				beq	 $11, $11, End 		#end of program (infinite loop)									#164
